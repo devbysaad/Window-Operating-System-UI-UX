e263dc18f3361abafe0da83b614a65a87d7c282e
devbysaad <dev.bysaad@gmail.com>
2025-10-06 18:53:00 +0500

    BAES

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import tailwindcss from '@tailwindcss/postcss'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base:process.env.VITE_BASE_PATH || "/Window-Operating-System-UI-UX"
+  // base:process.env.VITE_BASE_PATH || "/Window-Operating-System-UI-UX"
 })
